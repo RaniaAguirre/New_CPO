d2d472e4a9484a2593187f7b79d9bda94b6fbe8c
Alexander Sotomayor <alex15spineda@hotmail.com>
2025-03-29 09:11:02 -0600

    testing the backtesting

--- backtesting_test.py
+++ backtesting_test.py
@@ -1,38 +1,41 @@
 import pandas as pd
 import pickle
-from backtesting import BacktestMultiStrategy  
+from backtesting import BacktestMultiStrategy
 
-prices = pd.read_csv("prices.csv", index_col=0, parse_dates=True)
-indicators = pd.read_csv("Data_Base/Data_Base.csv", index_col=0, parse_dates=True)
+# === Cargar el dataset combinado ===
+data = pd.read_csv("Backtesting_data.csv", index_col=0, parse_dates=True)
 
-# Alinear fechas
-indicators.index = indicators.index.to_period("M").to_timestamp("M")
+# === Definir columnas ===
+price_cols = data.columns[1:16].tolist()  # columnas 2 a 16 (15 activos)
+indicator_cols = [col for col in data.columns if col not in price_cols]
 
-# Seleccionar 5 activos
-selected_assets = prices.columns[:5].tolist()
+# === Crear estructura MultiIndex ===
+price_multi = pd.concat([data[price_cols]], axis=1, keys=["Price"])
+ind_multi = pd.concat([data[indicator_cols]], axis=1, keys=["Indicator"])
+combined_data = pd.concat([price_multi, ind_multi], axis=1)
 
-# Crear estructura multi-nivel
-price_multi = pd.concat([prices[selected_assets]], axis=1, keys=["Price"])
-ind_multi = pd.concat([indicators], axis=1, keys=["Indicator"])
-
-# Alinear índices comunes
-common_index = price_multi.index.intersection(ind_multi.index)
-combined_data = pd.concat([price_multi.loc[common_index], ind_multi.loc[common_index]], axis=1)
-
-
+# === Cargar modelo SVR ===
 with open("trained_models/Dataset_1_SVR.pkl", "rb") as f:
     svr_model = pickle.load(f)
 
-svr_models = {'high_cap': svr_model, 'mid_cap': svr_model, 'low_cap': svr_model}
-xgboost_model = svr_model  
+# === Cargar modelo XGBoost entrenado ===
+with open("trained_models/Dataset_1_XGBoost_Sharpe.pkl", "rb") as f:
+    xgboost_model = pickle.load(f)
 
+# Diccionarios para pasar a la clase
+svr_models = {'high_cap': svr_model, 'mid_cap': svr_model, 'low_cap': svr_model}
+
+# === Seleccionar activos ===
+selected_assets = price_cols
+
+# === Clase personalizada ===
 class CustomBacktest(BacktestMultiStrategy):
     def select_assets(self, date):
         return selected_assets
     def get_cap_type(self, date):
         return 'high_cap'
 
-
+# === Ejecutar simulación ===
 bt = CustomBacktest(combined_data, svr_models, xgboost_model)
 bt.simulate()
 
